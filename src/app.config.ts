8b67c0522611cc38aba0bebb66b27784ca5a59e5
Seadtn <nasraminefsg@gmail.com>
2025-01-25 13:03:44 +0100

    feat: :sparkles: first commit with lando,g page

---
+++ src/app.config.ts
@@ -0,0 +1,63 @@
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { appRoutes } from './app.routes';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { 
+  TranslateModule, 
+  TranslateLoader, 
+  TranslateCompiler, 
+  TranslateParser, 
+  TranslateDefaultParser,
+  MissingTranslationHandler,
+  FakeMissingTranslationHandler
+} from '@ngx-translate/core';
+import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
+import { from } from 'rxjs';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideRouter(appRoutes, 
+            withInMemoryScrolling({ 
+                anchorScrolling: 'enabled', 
+                scrollPositionRestoration: 'enabled' 
+            }), 
+            withEnabledBlockingInitialNavigation()
+        ),
+        provideHttpClient(withFetch()),
+        provideAnimationsAsync(),
+        providePrimeNG({ 
+            theme: { 
+                preset: Aura, 
+                options: { darkModeSelector: '.app-dark' } 
+            } 
+        }),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            },
+            compiler: {
+                provide: TranslateCompiler,
+                useClass: TranslateMessageFormatCompiler
+            },
+            parser: {
+                provide: TranslateParser,
+                useClass: TranslateDefaultParser
+            },
+            missingTranslationHandler: {
+                provide: MissingTranslationHandler,
+                useClass: FakeMissingTranslationHandler
+            },
+            defaultLanguage: 'fr'
+        }).providers || []
+    ]
+};
